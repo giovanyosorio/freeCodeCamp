e55683490549ce748141b28c2c7fc0d64a0cc771
giovanyosorio <giovany.torres.799@gmail.com>
2023-10-07 20:04:10 -0500

    responsive design

--- freeCodeCamp-landing/src/App.jsx
+++ freeCodeCamp-landing/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
         <Testimonio
         name="Shawn Wang"
         country="Singapore"
-        imagen=""
+        imagen="https://www.freecodecamp.org/static/Shawn-bf42c7b909dae3111d18a21870ee6ca5.png"
         cargo="Software Engineer"
         empresa="Amazon"
         testimonio="It's scary to change careers. I only gained confidence that I could code by working through the hundreds of hours of free lessons on freeCodeCamp. Within a year I had a six-figure job as a Software Engineer. freeCodeCamp changed my life."
@@ -24,7 +24,7 @@ function App() {
       <Testimonio
         name="Sarah Chima"
         country="Nigeria"
-        imagen=""
+        imagen="https://www.freecodecamp.org/static/Sarah-45de0e53b7b2520ebbac35c454b9748a.png"
         cargo="Software Engineer"
         empresa="ChatDesk"
         testimonio="freeCodeCamp was the gateway to my career as a software developer. The well-structured curriculum took my coding knowledge from a total beginner level to a very confident level. It was everything I needed to land my first dev job at an amazing company."
@@ -34,7 +34,7 @@ function App() {
       <Testimonio
         name="Emma Bostian"
         country="Sweden"
-        imagen=""
+        imagen="https://www.freecodecamp.org/static/Emma-5a662db9bcf55809a1fefb2ea17634bc.png"
         cargo="Software Engineer"
         empresa="Spotify"
         testimonio="I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify."
--- freeCodeCamp-landing/src/components/Testimonio.jsx
+++ freeCodeCamp-landing/src/components/Testimonio.jsx
@@ -1,14 +1,15 @@
 import React from "react"
 import  "../stylesheets/Testimonio.css"
-
+//import Emma from "../src/imagenes/testimonio-Emma.png"
+//import shawn from "../src/imagenes/testimonio-shawn.png"
 function Testimonio(props){  
     return(
         <div className="contenedor-testimonio">
-            <img className="imagen-testimonio" src="https://www.freecodecamp.org/static/Emma-5a662db9bcf55809a1fefb2ea17634bc.png" alt="Foto de Emma" />
+            <img className="imagen-testimonio" src={props.imagen} alt="Foto de Emma" />
 
             <div className="contenedor-texto-testimonio">
-                <p className="nombre-testimonio"> {props.name} en {props.country}</p>
-                <p className="cargo-testimonio">{props.cargo} en {props.empresa}</p>
+                <p className="nombre-testimonio"> <strong>{props.name}</strong> en {props.country}</p>
+                <p className="cargo-testimonio">{props.cargo} en <strong>{props.empresa}</strong> </p>
                 <p className="texto-testimonio"> {props.testimonio}</p>
             </div>
         </div>
